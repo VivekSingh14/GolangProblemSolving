10f30a1e50dd2278e5a73ac612b54f5139cab8e8
Vivek Singh <viveksingh@Viveks-MacBook-Air.local>
2022-07-07 23:03:57 +0530

    chore: added annagram program

--- ProblemSolvingUpdated/rabinKarpAlgo.go
+++ ProblemSolvingUpdated/rabinKarpAlgo.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import "fmt"
 
+const MAX1 = 256
+
+func main() {
+	txt := "BACDGABCDA"
+	pat := "ABCD"
+
+	searchAnagram(txt, pat)
+}
+
+func searchAnagram(txt string, pat string) {
+	lenP := len(pat)
+	lenT := len(txt)
+
+	var countP [MAX1]rune
+	var countTW [MAX1]rune
+
+	for i := 0; i < lenP; i++ {
+		countP[pat[i]]++
+		countTW[txt[i]]++
+	}
+
+	for i := lenP; i < lenT; i++ {
+		if compareOne(countP, countTW) {
+			fmt.Println("Anangram is at", i-lenP)
+		}
+		countTW[txt[i]]++
+		countTW[txt[i-lenP]]--
+
+	}
+	if compareOne(countP, countTW) {
+		fmt.Println("Anagram found at: ", lenT-lenP)
+	}
+}
+
+func compareOne(countP [MAX1]rune, countTW [MAX1]rune) bool {
+	for i := 0; i < len(countP); i++ {
+		if countP[i] != countTW[i] {
+			return false
+		}
+	}
+	return true
 }
