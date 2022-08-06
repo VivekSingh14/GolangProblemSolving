a9af8bd1f1e503d8872bfba21e20546c5be8210b
Vivek Singh <viveksingh@Viveks-MacBook-Air.local>
2022-08-06 22:35:51 +0530

    chore: updated bst

--- DataStructures/BST.go
+++ DataStructures/BST.go
@@ -10,29 +10,30 @@ type BsTree struct {
 	root *Node1
 }
 
-func (b *BsTree) insert(data int) *BsTree {
+func (b *BsTree) insert(data int) {
 	node := Node1{}
 	if b.root == nil {
 		node.data = data
 		node.left = nil
 		node.right = nil
 		b.root = &node
-	} else {
-		b.root.insert(data)
-	}
-	return b
-}
-
-func (n *Node1) insert(data int) {
-	node := Node1{}
-	if n == nil {
 		return
-	} else if data <= n.data {
-		if n.left == nil {
-			node.data = data
-			node.left = nil
-			node.right = nil
-		}
 	}
+	//ptr := b.root
 
+	//return
 }
+
+// func (n *Node1) insert(data int) {
+// 	node := Node1{}
+// 	if n == nil {
+// 		return
+// 	} else if data <= n.data {
+// 		if n.left == nil {
+// 			node.data = data
+// 			node.left = nil
+// 			node.right = nil
+// 		}
+// 	}
+
+// }
