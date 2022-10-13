782b6ded8e71d7aba0a5ea6eca155a5cb410a700
Vivek Singh <vivek.singh141@yahoo.com>
2022-10-13 07:47:33 +0530

    chore: created the max heap and insertion method body is created

--- DataStructures/BinaryHeap.go
+++ DataStructures/BinaryHeap.go
@@ -1,17 +1,29 @@
 package main
 
-type BinaryHeap struct {
-	left  *BinaryHeap
-	data  int
-	right *BinaryHeap
-}
-
-func InsertInHeap(root *BinaryHeap, data int) *BinaryHeap {
-
-	return nil
-
-}
+import "fmt"
 
 func main() {
+	heapArr := make([]int, 0)
+	heapArr = append(heapArr, 0)
+	heapArr = append(heapArr, 40)
+	heapArr = append(heapArr, 35)
+	heapArr = append(heapArr, 15)
+	heapArr = append(heapArr, 30)
+	heapArr = append(heapArr, 10)
+	heapArr = append(heapArr, 12)
+	heapArr = append(heapArr, 6)
+	heapArr = append(heapArr, 5)
+	heapArr = append(heapArr, 20)
 
+	//to be inserted in heap
+	heapArr = append(heapArr, 50)
+
+	fmt.Println(heapArr)
+	fmt.Println(len(heapArr))
+	fmt.Println(cap(heapArr))
+}
+
+func InsertInHeap(arr []int, key int) []int {
+
+	return nil
 }
