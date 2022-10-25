118f802fe2e6cb731ec205314571a3bb5f25de24
Vivek Singh <vivek.singh141@yahoo.com>
2022-10-25 11:58:16 +0530

    chore: added undirected graph adjcency matrix representation

--- GraphLearn/Graph1.go
+++ GraphLearn/Graph1.go
@@ -1,6 +1,54 @@
 package main
 
-//the representation of graph using matrics
+import "fmt"
+
+//the representation of graph using  adjacency matrics
+// graph representation
+// 1 ---- 2
+// |\     |
+// | \    |
+// |  \   |
+// |   \  |
+// |    \ |
+// |     \|
+// 4 ---- 3
+// \      /
+//  \    /
+//   \  /
+//     5
+
+// Undirected Graph
+// |V| = 5
+// |E| = 7
+
 func main() {
+	//NoOfVertices := 5
+	adjMat := [6][6]int{}
+
+	// for i := range adjMat {
+	// 	adjMat[i] = make([]int, NoOfVertices)
+	// }
+
+	adjMat[1][2] = 1
+	adjMat[2][1] = 1
+	adjMat[1][4] = 1
+	adjMat[4][1] = 1
+	adjMat[2][3] = 1
+	adjMat[3][2] = 1
+	adjMat[1][3] = 1
+	adjMat[3][1] = 1
+	adjMat[4][3] = 1
+	adjMat[3][4] = 1
+	adjMat[4][5] = 1
+	adjMat[5][4] = 1
+	adjMat[3][5] = 1
+	adjMat[5][3] = 1
+
+	for i := 0; i < len(adjMat); i++ {
+		for j := 0; j < len(adjMat[i]); j++ {
+			fmt.Print(adjMat[i][j], " ")
+		}
+		fmt.Println()
+	}
 
 }
