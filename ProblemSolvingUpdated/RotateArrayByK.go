422439cbd46416e3b52abce1013940767d842346
Vivek Singh <vivek.singh141@yahoo.com>
2023-01-14 13:08:20 +0530

    feat: added a method to rotate the array by k elements

---
+++ ProblemSolvingUpdated/RotateArrayByK.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//nums = [1,2,3,4,5,6,7]
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	k := 3
+
+	rotate(nums, k)
+
+}
+
+func rotate(nums []int, k int) {
+	max := len(nums) - 1
+	for i := 1; i <= k; i++ {
+		temp := nums[len(nums)-1]
+		for j := 0; j < len(nums)-1; j++ {
+			nums[max-j] = nums[max-(j+1)]
+		}
+		nums[0] = temp
+	}
+
+	fmt.Println(nums)
+}
