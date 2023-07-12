bb08fcbbce41775008ffefded4349b7eef121c84
Vivek Singh <vivek.singh141@yahoo.com>
2023-07-12 23:48:29 +0530

    feat: added a function to print first duplicate char from string

---
+++ easyExamples/firstDuplicateInString.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "1abBcdefcghi1fxyAtB"
+	fmt.Println(firstDuplicateChar(str))
+}
+
+func firstDuplicateChar(s string) string {
+	var arr [256]int
+
+	if len(s) == 1 {
+		return s
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			arr[s[i]] = arr[s[i]] + 1
+		} else if s[i] >= 'A' && s[i] <= 'Z' {
+			arr[s[i]] = arr[s[i]] + 1
+		} else {
+			arr[s[i]] = arr[s[i]] + 1
+		}
+	}
+	//fmt.Println(arr)
+
+	for i := 0; i < len(s); i++ {
+		if arr[s[i]] == 2 {
+
+			return string(s[i])
+		}
+	}
+	return ""
+}
--- easyExamples/search2DMatrix.go
+++ easyExamples/search2DMatrix.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main18() {
 
 	//matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}
 
