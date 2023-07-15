247e01eda8928800c943e6ce0242330a35477728
Vivek Singh <vivek.singh141@yahoo.com>
2023-07-15 23:34:08 +0530

    feat: added function to find out max profit in buying and selling stocks

---
+++ easyExamples/buySellStock1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	prices := []int{3, 2, 6, 5, 0, 3}
+
+	fmt.Println(maxProfit(prices))
+}
+
+func maxProfit(prices []int) int {
+	buy := prices[0]
+	max_profit := 0
+
+	for i := 1; i < len(prices); i++ {
+		if buy > prices[i] {
+			buy = prices[i]
+		} else if prices[i]-buy > max_profit {
+			max_profit = prices[i] - buy
+		}
+	}
+
+	return max_profit
+}
