3206ccdc983c61309a2011203ee7d0ea72ae1ad7
Vivek Singh <vivek.singh141@yahoo.com>
2023-07-26 23:32:58 +0530

    feat: added a function to count the segments in string

---
+++ easyExamples/countsegments.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Hello"
+	fmt.Println(countSegments(str))
+}
+
+func countSegments(s string) int {
+
+	if len(s) == 0 {
+		return 0
+	}
+
+	arr := strings.Split(s, " ")
+
+	return len(arr)
+}
