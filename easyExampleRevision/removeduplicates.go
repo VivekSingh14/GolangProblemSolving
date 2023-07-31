613f872d4791245a3cf657d3e667ae63f4694e91
Vivek Singh <vivek.singh141@yahoo.com>
2023-07-31 23:56:37 +0530

    feat: added a function to remove the duplicates

---
+++ easyExampleRevision/removeduplicates.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+
+	fmt.Println(removeDuplicates(nums))
+}
+
+func removeDuplicates(nums []int) int {
+
+	fast := 1
+	slow := 0
+
+	for fast < len(nums) {
+		if nums[fast] == nums[slow] {
+			fast++
+		} else if nums[fast] != nums[slow] {
+			nums[fast-1] = nums[fast]
+			slow++
+			fast++
+		}
+	}
+	fmt.Println(nums)
+	return slow - 1
+}
