6d034b24d876840c8e01597c4e9fe328b7b1bb9e
Vivek Singh <vivek.singh141@yahoo.com>
2023-12-03 04:43:08 +0530

    feat: added a method find out nth fibonacci term

---
+++ AimRevision/DynamicProgramming/Fibonacci.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	finalTerm := 5
+
+	arr := make([]int, finalTerm)
+
+	//base cases are created
+	arr[0] = 0
+	arr[1] = 1
+
+	for i := 2; i < finalTerm; i++ {
+		//using the previous result to get the new result
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+
+	fmt.Println(arr[finalTerm-1])
+
+}
