e42c4571b05206e61f550dafefe198b9645b1160
Vivek Singh <vivek.singh141@yahoo.com>
2023-12-11 23:38:16 +0530

    feat: added a function print group anagrams

---
+++ AimRevision/StringProblems/GroupAnagrams.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type data struct {
+	str []string
+}
+
+func main() {
+	arr := []string{"cat", "dog", "tac", "god", "act"}
+	groupAnagram(arr)
+}
+
+func groupAnagram(arr []string) {
+
+	map1 := make(map[int]data)
+	for i := 0; i < len(arr); i++ {
+		r := []rune(arr[i])
+		temp := 0
+		for j := 0; j < len(r); j++ {
+			temp += int(r[j])
+		}
+		if _, ok := map1[temp]; !ok {
+			var data1 []string
+			data1 = append(data1, string(r))
+			map1[temp] = data{data1}
+		} else {
+			res := map1[temp]
+			res.str = append(res.str, string(r))
+			map1[temp] = res
+		}
+	}
+
+	for _, value := range map1 {
+		fmt.Println(value)
+	}
+
+}
--- AimRevision/StringProblems/ValidParenthesis.go
+++ AimRevision/StringProblems/ValidParenthesis.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main8() {
 	str := "[()]{}{[()()]()}"
 	fmt.Println(ValidParentheses(str))
 }
