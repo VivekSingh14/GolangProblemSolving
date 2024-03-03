5e850086cf5284318223af49a39adb08c0351f40
Vivek Singh <vivek.singh141@yahoo.com>
2024-03-04 01:08:16 +0530

    added program to implement running sum using prefix sum alog

---
+++ AimRevision2/ArrayProblems/RunningSumOf1dArray.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/* --
+Prefix Sum algorithm
+-- */
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(runningSum(nums))
+}
+
+func runningSum(nums []int) []int {
+	res := make([]int, len(nums))
+	res[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		res[i] = res[i-1] + nums[i]
+	}
+	return res
+}
