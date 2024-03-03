f99b887fda7d3ea7a9a94f2dbb00bc9463e96c65
Vivek Singh <vivek.singh141@yahoo.com>
2024-03-04 00:19:19 +0530

    added program to implement insert into max heap

--- AimRevision2/HeapLearning/InsertInMaxHeap.go
+++ AimRevision2/HeapLearning/InsertInMaxHeap.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	heap := []int{0, 30, 20, 15, 5, 10, 12, 6}
+	element := 40
+	//res := InsertionIntoMaxHeap(heap, element)
+	fmt.Println(InsertionIntoMaxHeap(heap, element)[1:])
+
+}
+
+func InsertionIntoMaxHeap(arr []int, element int) []int {
+
+	arr = append(arr, element)
+	i := len(arr) - 1
+	for i > 1 {
+		if arr[i/2] < element {
+			arr[i] = arr[i/2]
+			arr[i/2] = element
+		}
+		i = i / 2
+	}
+
+	return arr
 }
